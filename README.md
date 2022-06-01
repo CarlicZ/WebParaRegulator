7cacfafa53ce03fa9c931300940ed4671bc1ff0b
smcbaq <zsh1282984748@163.com>
2022-06-02 00:12:13 +0800

    version 1.3_1 修改了客户端的问题和信息输出流，完成了README.md的部分编写

--- README.md
+++ README.md
@@ -1,9 +1,63 @@
 # WebParaRegulator
 
-Robomaster网页调参项目
+此为深圳大学RoboPilots网页调参项目
 
 [toc]
 
+# 项目简介
+
+要想使用本项目，需要关注以下三个主要程序：
+
+-   `data_api.py`：基于flask开发的服务器python程序
+-   `WebParaClient.py`：基于python的客户端程序
+-   `demo.py` ：基于python的模拟读程序流程
+
+其中 `demo.py` 详细说明了应用程序如何与 `WebParaClient.py` 交互的详细流程，可以自行设计自己应用中的流程来读取参数。
+
+项目交互的方式如下图所示：
+
+![网页调参工作原理](https://smcbaq-picture.oss-cn-guangzhou.aliyuncs.com/images_2/picgo/aliyunOSS%E7%BD%91%E9%A1%B5%E8%B0%83%E5%8F%82%E5%B7%A5%E4%BD%9C%E5%8E%9F%E7%90%86.jpg)
+
+其他重要文件：
+
+-   `car_conf.py `、`read_flag.py`：客户端交互文件
+-   `RP_car_data.db`：存储车辆参数数据库
+-   `index.html`、`car.html`：调参网页
+
 # 安装使用
-欸嘿嘿等我填坑吧嘎嘎嘎
+
+1. 依赖包安装：
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+2.   服务端安装：
+
+     方法1：直接运行（注意退出终端的进程结束问题）
+
+     ```bash
+     python data_api.py
+     ```
+
+     方法2：使用`systemd`进行进程守护（linux下）
+
+     1.   修改 `systemd/WebParaServer.service` 文件中的**路径**和**用户名**，路径请务必使用绝对路径
+     2.   将`systemd/WebParaServer.service` 文件复制到 `/etc/systemd/system`文件夹下
+     3.   使用命令 `sudo systemctl start WebParaServer`启动服务
+     4.   若修改了 `*.service` 文件重新复制，需要使用 `sudo systemctl daemon-reload` 重载一下
+
+3.   客户端安装：
+
+     与服务端方法2差不多，使用`systemd`进行进程守护（linux下）
+
+     1.   修改 `systemd/WebParaClient.service` 文件中的**路径**和**用户名**，路径请务必使用绝对路径
+     2.   将`systemd/WebParaClient.service` 文件复制到 `/etc/systemd/system`文件夹下
+     3.   使用命令 `sudo systemctl start WebParaClient`启动服务
+     4.   若修改了 `*.service` 文件重新复制，需要使用 `sudo systemctl daemon-reload` 重载一下
+     5.   可选：使用命令 `sudo systemctl enable WebParaClient`启动开机自启
+
+# 效果
+
+使用界面还未优化完成，待填坑
 
--- WebParaClient.py
+++ WebParaClient.py
@@ -17,39 +17,60 @@ from time import sleep
 @click.option("--port", '-p', help="Port of the service on the host", default="5000")
 @click.option("--car", '-c', help="Name of the car to get conf", required=True)
 @click.option("--to", '-t', help="Conf file output path", default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'car_conf.json'))
-@click.option("--retry", '-r', help="Retry to get access to the api or not", default=False)
+@click.option("--retry", '-r', help="Retry to get access to the api or not", default=True)
 @click.option("--flag", '-f', help="Flag file path", default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'read_flag.json'))
 def WebParaClient(gap, host, port, car, to, retry, flag): 
     while True:
+        Http_flag = True # 用于标志http请求是否正常
+
         Car_para_json = {}
-        try: 
         # 检查read_flag.json是否需要重置车辆参数更新位
-            if  not os.path.exists(flag):
-                f_tmp=open(flag,"w")
-                f_tmp.close()
-            flag_f = open(flag, "r")
-            try:
-                flag_json = json.load(flag_f)
-            except:
-                flag_json={}
-            if 'Read_flag' not in flag_json:
-                flag_json['Read_flag']=False
-            # 程序已经读取完了参数，说明服务端的数据没有更新
-            if flag_json['Read_flag'] == True: 
-                print("[info] reset car")
+        if not os.path.exists(flag):
+            f_tmp=open(flag,"w")
+            f_tmp.close()
+        flag_f = open(flag, "r")
+        try:
+            flag_json = json.load(flag_f)
+        except:
+            flag_json={}
+        if 'Read_flag' not in flag_json:
+            flag_json['Read_flag']=False
+        # 程序已经读取完了参数，说明服务端的数据没有更新
+        if flag_json['Read_flag'] == True: 
+            print("[info] Reseting car")
+            try: 
                 response = requests.post("http://" + host +":" + port + "/api/reset_Car", data={"name": car})
                 print(response.text)
-            flag_f.close()
-
+                print("[info] Reset car success!")
+            except: 
+                print("\033[31m[err] Reset car failed!\033[0m")
+                Http_flag = False
+        flag_f.close()
+        try: 
             # 获取车辆状态
+            print("[info] Getting car Update_flag")
             Car_state = requests.get("http://" + host +":" + port + "/api/get_Cars")
             Car_para_json["Update_flag"] = json.loads(Car_state.text)[car]["Update_flag"]
+            print("[info] Getting car Update_flag success!")
             
             # 获取车辆参数
+            print("[info] Getting car paras")
             Car_para = requests.get("http://" + host +":" + port + "/api/get_Paras/" + car)
-            print("http://" + host +":" + port + "/api/get_Paras/" + car)
-            tmp_json = json.loads(Car_para.text)
+            print("[info] Getting car paras")
+        except: 
+            print("\033[31m[err] Getting car Updata_flag or para failed!\033[0m")
+            Http_flag = False
+        
+
+        # HTTP请求标志检查，如果不正常，本次请求流程作废，不更新参数文件
+        if not Http_flag: 
+            print("\033[31m[err]Get access to api WRONG\033[0m")
+            if not retry: 
+                sys.exit(1)
+        # HTTP请求标志正常，更新参数文件
+        else: 
             # 对参数作类别分析
+            tmp_json = json.loads(Car_para.text)
             for i in tmp_json: 
                 if tmp_json[i]['Para_type'].lower() == "int": 
                     Car_para_json[i] = int(tmp_json[i]['Para_val'])
@@ -58,20 +79,15 @@ def WebParaClient(gap, host, port, car, to, retry, flag):
                 else: 
                     Car_para_json[i] = tmp_json[i]['Para_val']
             print(Car_para_json)
-        except: 
-            print("Get access to api WRONG")
-            if not retry: 
+            # 写文件
+            try:
+                to_f = open(to, "w")
+                json.dump(Car_para_json, to_f, ensure_ascii=False, indent=4)
+                to_f.close()
+            except: 
+                print("write json fail!")
+                to_f.close()
                 sys.exit(1)
-        
-        
-        try:
-            to_f = open(to, "w")
-            json.dump(Car_para_json, to_f, ensure_ascii=False, indent=4)
-            to_f.close()
-        except: 
-            print("write json fail!")
-            to_f.close()
-            sys.exit(1)
         sleep(gap)
 
 
